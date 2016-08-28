c098feab3dc2137da5c7e617f0fd038eaced8e10
Inseo B <yunibaek@hotmail.com>
2016-08-29 01:50:21 +0200

    Simplify and reskin AddEvent page

--- src/AddEvent.js
+++ src/AddEvent.js
@@ -13,10 +13,48 @@ import {
   Switch,
   Animated,
 } from 'react-native'
-import styles from './styles'
+import styles from './AddEvent.styles'
+
+import Icons from './Icons'
 
 //Navigation Bar Library
 import NavigationBar from 'react-native-navbar'
+
+function HighlighedIcon(props) {
+  var style = [styles.HighlighedIcon];
+  if(props.active) {
+    style = style.concat(styles.HighlighedIconActive);
+  }
+  return (
+  <View style={style}>
+    {props.children}
+  </View>
+  );
+}
+
+function DateTypeGrid(props) {
+  const rowStyle = {flowDirection: "row",};
+  return (
+    <View>
+      <View style={{flexDirection: 'row', flexWrap: 'nowrap', justifyContent: "space-between"}}>
+        <HighlighedIcon active={props.sex} >
+          <Icons.Sex inverted={props.sex} onPress={() => props.onTypeChange({sex: !props.sex})} />
+        </HighlighedIcon>
+        <HighlighedIcon active={props.fight} >
+          <Icons.Fight inverted={props.fight} onPress={() => props.onTypeChange({fight: !props.fight})} />
+        </HighlighedIcon>
+      </View>
+      <View style={{flexDirection: 'row', flexWrap: 'nowrap', justifyContent: "space-around"}}>
+        <HighlighedIcon active={props.nightIn} >
+          <Icons.NightIn inverted={props.nightIn} onPress={() => props.onTypeChange({nightIn: !props.nightIn})} />
+        </HighlighedIcon>
+        <HighlighedIcon active={props.nightOut} >
+          <Icons.NightOut inverted={props.nightOut} onPress={() => props.onTypeChange({nightOut: !props.nightOut})} />
+        </HighlighedIcon>
+      </View>
+    </View>
+  );
+}
 
 export default class AddEvent extends Component {
   constructor (props) {
@@ -24,42 +62,47 @@ export default class AddEvent extends Component {
     this.state = {
       notes: "",
 
-    }
-  }
+      sex: false,
+      fight: true,
+      nightIn: true,
+      nightOut: false,
 
-  state = {
-    datefalseSwtichIsOn: false,
-    sexfalseSwtichIsOn: false,
+      datefalseSwtichIsOn: false,
+      sexfalseSwtichIsOn: false,
+    }
   }
 
   onNotes(str) {
     this.setState({notes: str});
   }
 
+  onTypeChange(change) {
+    this.setState({
+      ...change,
+    });
+  }
 
-    render () {
+  render () {
   return (
     <View style = {{flex: 1}}>
-    <NavigationBar
-      title = {{title: 'Add Event', }}
-      leftButton = {{title : 'Cancel', tintColor: '#FF4981', handler:() => this.props.navigator.pop() }}
-      rightButton = {{title: 'Done', tintColor: '#FF4981', handler:() => this.props.navigator.pop()
-   }}
+      <NavigationBar
+        title = {{title: 'Add Event', }}
+        leftButton = {{title : 'Cancel', tintColor: '#FF4981', handler:() => this.props.navigator.pop() }}
+        rightButton = {{title: 'Done', tintColor: '#FF4981', handler:() => this.props.navigator.pop() }}
       />
 
-    <Text> Date Night </Text>
-             <Switch
-        onValueChange = {(value) => this.setState({datefalseSwtichIsOn: value})}
-        value = {this.state.datefalseSwtichIsOn}
-        onTintColor = "#FF4981"/>
+      <DateTypeGrid onTypeChange={this.onTypeChange.bind(this)} {...this.state} />
 
-      <Text> Sexual Relations</Text>
-            <Switch
-        onValueChange = {(value) => this.setState({sexfalseSwtichIsOn: value})}
-        value = {this.state.sexfalseSwtichIsOn}
-        onTintColor = "#FF4981"/>
-        <View value = {this.state.notes}>
+      <View value = {this.state.notes}>
       <Text> Notes </Text>
+      <View style={styles.Notes}>
+        <TextInput
+          style={styles.NotesInput}
+
+          multiline={true}
+          placeholder={"Notes about the day ...\n\nRemember the best parts :)"}
+        />
+      </View>
       <TextInput style = {styles.nameInput} placeholder= "Time and Location" value={this.state.notes} onChangeText={(str) => this.onNotes(str)}/>
       </View>
     </View>
---
+++ src/AddEvent.styles.js
@@ -0,0 +1,32 @@
+import React from 'react-native'
+
+export default React.StyleSheet.create({
+  HighlighedIcon: {
+    flex: 1,
+    margin: 10,
+    marginTop: 5,
+    marginBottom: 5,
+    borderRadius: 5,
+  },
+  HighlighedIconActive: {
+    backgroundColor: '#FF4981',
+  },
+  DateTypeRow: {
+
+  },
+
+  Notes: {
+    borderRadius: 5,
+    borderColor: '#D8D8D8',
+    borderWidth: 1,
+    margin: 10,
+  },
+  NotesInput: {
+    padding: 5,
+    minHeight: 120,
+    flex: 1,
+    color: '#bbb',
+    fontSize: 16,
+  }
+});
+
