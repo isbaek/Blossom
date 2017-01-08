bd8868d2706133801c3e0e3f5fead1d33437ea83
Inseo B <yunibaek@hotmail.com>
2017-01-08 22:24:15 +0100

    reducer: make inserting events unique and add DELETE_EVENT action type

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,6 +7,8 @@
     RESET_ALL,
 } from '../constants/ActionTypes'
 
+import moment from 'moment'
+
 const initialState = {
     couple: {
         you: {
@@ -30,6 +32,10 @@ const initialState = {
     ],
 };
 
+function isSameDay(d1, d2) {
+    return moment(d1).format('DD/MM/YYYY') === moment(d2).format('DD/MM/YYYY')
+}
+
 export default function reducer(state = initialState, action) {
     if(action.type == EDIT_COUPLE) {
         return {
@@ -39,7 +45,22 @@ export default function reducer(state = initialState, action) {
     } else if(action.type == ADD_EVENT) {
         return {
             ...state,
-            events: state.events.concat(action.payload.event),
+            events: state.events
+            // Remove previous event (if any) on same day
+            // because we don't want more than one event per day
+            .filter((event) => {
+                return !isSameDay(event.date, action.payload.event.date);
+            })
+            // Add new event data
+            .concat(action.payload.event),
+        };
+    } else if(action.type == DELETE_EVENT) {
+        return {
+            ...state,
+            // Remove event on a given day
+            events: state.events.filter((event) => {
+                return !isSameDay(event.date, payload.date);
+            })
         };
     }
     if(action.type == RESET_ALL) {
